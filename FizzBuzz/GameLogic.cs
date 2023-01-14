fa45d1d14a9475556b56aeea0f670b142a6687bb
kamil witkowski <kamilwitkowski5@gmail.com>
2023-01-14 16:05:27 +0100

    Add project files.

---
+++ FizzBuzz/GameLogic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    internal class GameLogic
+    {
+        public string StartGame(int userNumber)
+        {
+            
+
+            if (userNumber % 3 == 0 && userNumber % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (userNumber%5==0)
+            {
+                return "Buzz";
+            }
+            else if (userNumber % 3 == 0)
+            {
+                return "Fizz";
+
+            }
+            return userNumber.ToString();
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Czesć! Podaj liczbę");
+
+            var userNumber = UserNumber();
+
+            GameLogic gameLogic = new GameLogic();
+
+            Console.WriteLine(gameLogic.StartGame(userNumber));
+        }
+        private static int UserNumber()
+        {
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int userNumber))
+                {
+                    Console.WriteLine("Podano zły znak.\nPodaj liczbę");
+                    continue;
+                }
+                else
+                {
+                    return userNumber;
+                }
+            }
+        }
+    }
+}
